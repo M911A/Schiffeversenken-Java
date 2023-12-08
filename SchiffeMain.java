9bc48a412f909cc67bba7d0aeb332e754822ea13
mika <mika.griebsch@gmail.com>
2023-12-08 09:34:49 +0100

    Kommentare hinzugefügt und Überprüfung, ob Bombe im Spielbereich ist

--- SchiffeMain.java
+++ SchiffeMain.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 
 public class SchiffeMain {
+    //Initiialisierung Variablen und Arrays
     private String[][] spielfeld1 = new String[5][5];
     private String[][] spielfeld2 = new String[5][5];
     private String[][] anzeigeFeld1 = new String[5][5];
     private String[][] anzeigeFeld2 = new String[5][5];
-    private int[] uebrig = new int[2];
     int anzahlBoote = 2;
-
+    int uebrigeBooteSpieler1 = anzahlBoote; 
+    int uebrigeBooteSpieler2 = anzahlBoote; 
+    
     public static void main(String[] args) {
         SchiffeMain main = new SchiffeMain();
 
-        main.run();
+        main.run(); //alles in run ausgelagert
 
     }
 
-    private void run() {
+    private void run() { //"Main-Methodex" 
         init();
         printAnzeigeFelder();
         bootePlatzieren();
@@ -23,20 +25,18 @@ public class SchiffeMain {
         bombenPlatzieren();
     }
 
-    private void init() {
+    private void init() { //Befüllen der Arrays für die Spielfelder in init Klasse ausgelagert
         anzeigeFelderInit();
         spielFelderInit();
-        uebrig[0] = anzahlBoote-1;
-        uebrig[1] = anzahlBoote-1;
     }
 
     // Spielfeld
-    private void spielFelderInit() {
+    private void spielFelderInit() { //Aufabu der Spielfelder -- Befüllen
         spielfeldInit1();
         spielfeldInit2();
     }
 
-    private void spielfeldInit1() {
+    private void spielfeldInit1() { //Befüllen eines Einzelnen Spielfeldes
         for (int i = 0; i < spielfeld1.length; i++) {
             for (int j = 0; j < spielfeld1[i].length; j++) {
                 spielfeld1[i][j] = "0";
@@ -52,10 +52,10 @@ public class SchiffeMain {
         }
     }
 
-    // private void printSpielfelder() {
-    //     printSpielfeld1(spielfeld1);
-    //     printSpielfeld2(spielfeld2);
-    // }
+    private void printSpielfelder() { //Anzeigen der Spielfelder nur notwendig wärend der Entwicklung
+        printSpielfeld1(spielfeld1);
+        printSpielfeld2(spielfeld2);
+    }
 
     private void printSpielfeld1(String[][] spielfeld1) {
         System.out.println("Spieler 1:");
@@ -87,7 +87,7 @@ public class SchiffeMain {
     }
     // Spielfeld
 
-    // Anzeige Feld
+    // Anzeige Feld  -- Funktion gleich den Spielfeldern
     private void anzeigeFelderInit() {
         anzeigeFeldInit1();
         anzeigeFeldInit2();
@@ -115,7 +115,7 @@ public class SchiffeMain {
     }
 
     private void printAnzeigeFeld1(String[][] anzeigeFeld1) {
-        System.out.println("Spieler 1:\t Übrige Boote: " + uebrig[0]);
+        System.out.println("Spieler 1:\t Übrige Boote: " + uebrigeBooteSpieler1);
         for (String[] row : anzeigeFeld1) {
             for (String cell : row) {
                 System.out.print(cell + " ");
@@ -132,121 +132,137 @@ public class SchiffeMain {
             }
             System.out.println();
         }
-        System.out.println("Spieler 2:\t Übrige Boote: " + uebrig[1]);
+        System.out.println("Spieler 2:\t Übrige Boote: " + uebrigeBooteSpieler2);
     }
     // Annzeige Feld
 
     // Boote platzieren
-    private void bootePlatzieren() {
+    private void bootePlatzieren() { //Auslagern von 2 Methoden zu einer
         booteSpieler1();
         booteSpieler2();
     }
 
-    private void booteSpieler1() {
+    private void booteSpieler1() {//Platzieren der Botte von Spieler 1 
         Scanner scan = new Scanner(System.in);
         int boote = 1;
         while (boote < anzahlBoote + 1) {
-            System.out.println("Boot " + boote + " platzieren (Reihe Spalte): ");
+            System.out.println("Boot " + boote + " platzieren (Reihe / Enter / Spalte): ");
             int row = scan.nextInt();
             int col = scan.nextInt();
 
             if (positionVergebenSpieler1(row, col)) {
-                spielfeld1[row][col] = "B"; // Markiere die Position mit "B" für Boot
+                spielfeld1[row][col] = "B"; // Markierung der Position mit "B" für Boot im Spielfeld
                 boote++;
             } else {
                 System.out.println("Ungültige Position. Bitte wähle andere Koordinaten.");
             }
         }
+        scan.close();
     }
 
-    private void booteSpieler2() {
+    private void booteSpieler2() {//Platzieren der Botte von Spieler 2 
         Scanner scan = new Scanner(System.in);
         int boote = 1;
         while (boote < anzahlBoote + 1) {
-            System.out.println("Boot " + boote + " platzieren (Reihe Spalte): ");
+            System.out.println("Boot " + boote + " platzieren (Reihe / Enter /Spalte): ");
             int row = scan.nextInt();
             int col = scan.nextInt();
 
             if (positionVergebenSpieler2(row, col)) {
-                spielfeld2[row][col] = "B"; // Markiere die Position mit "B" für Boot
+                spielfeld2[row][col] = "B"; // Markierung der Position mit "B" für Boot im Spielfeld
                 boote++;
             } else {
                 System.out.println("Ungültige Position. Bitte wähle andere Koordinaten.");
             }
         }
+        scan.close();
     }
 
-    private boolean positionVergebenSpieler1(int row, int col) {
+    private boolean positionVergebenSpieler1(int row, int col) { //Überprüfung ob eine gültige Position Verwende weird
         return row >= 0 && row < spielfeld1.length && col >= 0 && col < spielfeld1[0].length
                 && spielfeld1[row][col].equals("0");
     }
 
-    private boolean positionVergebenSpieler2(int row, int col) {
+    private boolean positionVergebenSpieler2(int row, int col) { //Überprüfung ob eine gültige Position Verwende weird
         return row >= 0 && row < spielfeld2.length && col >= 0 && col < spielfeld2[0].length
                 && spielfeld2[row][col].equals("0");
     }
     // Boote platzieren
 
     // Bombe Platzieren
-    private void bombenPlatzieren() {
+    private void bombenPlatzieren() {//Bomben platziern bie ein Spieler gewonnen hat
         int gefundenSpieler1 = 0;
         int gefundenSpieler2 = 0;
         boolean ende = false;
         Scanner scan = new Scanner(System.in);
         do {
             System.out.println();
+
+            //Spieler 2 Bombe platzieren 
             System.out.println("Bombe Spieler 1: ");
             int rowSpieler1 = scan.nextInt();
             int colSpieler1 = scan.nextInt();
-            if (bombeTrifftBootSpieler1(rowSpieler1, colSpieler1)) {
-                anzeigeFeld2[rowSpieler1][colSpieler1] = "\u001B[32mX\u001B[0m";
-                printAnzeigeFelder();
-                uebrig[0]--;
-                gefundenSpieler1++;
-                if (gefundenSpieler1 == anzahlBoote) {
+            if (bombeAufSpielfeld(rowSpieler1, colSpieler1)) {
+                if (bombeTrifftBootSpieler1(rowSpieler1, colSpieler1)) {
+                    anzeigeFeld2[rowSpieler1][colSpieler1] = "\u001B[32mX\u001B[0m";
                     printAnzeigeFelder();
-                    System.out.println("Spieler 1 hat gewonnen!");
-                    break;
+                    uebrigeBooteSpieler2--;
+                    gefundenSpieler1++;
+                    if (gefundenSpieler1 == anzahlBoote) {
+                        printAnzeigeFelder();
+                        System.out.println("Spieler 1 hat gewonnen!");
+                        break;
+                    }
+                } else {
+                    anzeigeFeld2[rowSpieler1][colSpieler1] = "\u001B[31m*\u001B[0m";
+                    printAnzeigeFelder();
                 }
-            } else {
-                anzeigeFeld2[rowSpieler1][colSpieler1] = "\u001B[31m*\u001B[0m";
-                printAnzeigeFelder();
+            }
+            else {
+                System.out.println("Ungültige Position. Bitte wähle beim nächsten mal eine andere Koordinaten.");
             }
             
+            //Spieler 2 Bombe platzieren 
             System.out.println();
             System.out.println("Bombe Spieler 2: ");
             int rowSpieler2 = scan.nextInt();
             int colSpieler2 = scan.nextInt();
-            if (bombeTrifftBootSpieler2(rowSpieler2, colSpieler2)) {
-                anzeigeFeld1[rowSpieler2][colSpieler2] = "\u001B[32mX\u001B[0m";
-                printAnzeigeFelder();
-                uebrig[1]--;
-                gefundenSpieler2++;
-                if (gefundenSpieler2 == anzahlBoote) {
+            if (bombeAufSpielfeld(rowSpieler2, colSpieler2)) {
+                if (bombeTrifftBootSpieler2(rowSpieler2, colSpieler2)) {
+                    anzeigeFeld1[rowSpieler2][colSpieler2] = "\u001B[32mX\u001B[0m";
                     printAnzeigeFelder();
-                    System.out.println("Spieler 2 hat gewonnen!");
-                    break;
+                    uebrigeBooteSpieler1--;
+                    gefundenSpieler2++;
+                    if (gefundenSpieler2 == anzahlBoote) {
+                        printAnzeigeFelder();
+                        System.out.println("Spieler 2 hat gewonnen!");
+                        break;
+                    }
+                } else {
+                    anzeigeFeld1[rowSpieler2][colSpieler2] = "\u001B[31m*\u001B[0m";
+                    printAnzeigeFelder();
                 }
-            } else {
-                anzeigeFeld1[rowSpieler2][colSpieler2] = "\u001B[31m*\u001B[0m";
-                printAnzeigeFelder();
+            }
+            else {
+                System.out.println("Ungültige Position. Bitte wähle beim nächsten mal eine andere Koordinaten.");
             }
 
         } while (!ende);
-        scan.close(); // Schließe den Scanner, wenn er nicht mehr benötigt wird
+        scan.close();
     }
 
-    private boolean bombeTrifftBootSpieler1(int row, int col) {
+    private boolean bombeTrifftBootSpieler1(int row, int col) { //Überprüfen, ob Bombe Schwiff trifft 
         return row >= 0 && row < spielfeld1.length && col >= 0 && col < spielfeld1[0].length
-                && spielfeld1[row][col].equals("B"); // Überprüfe, ob die Position im Spielfeld ein Boot ist
+                && spielfeld1[row][col].equals("B"); 
     }
-
-    private boolean bombeTrifftBootSpieler2(int row, int col) {
+    private boolean bombeTrifftBootSpieler2(int row, int col) { //Überprüfen, ob Bombe Schwiff trifft 
         return row >= 0 && row < spielfeld2.length && col >= 0 && col < spielfeld2[0].length
-                && spielfeld2[row][col].equals("B"); // Überprüfe, ob die Position im Spielfeld ein Boot ist
+        && spielfeld2[row][col].equals("B"); 
+    }
+    private boolean bombeAufSpielfeld(int row, int col) { //Überprüfen, ob Bombe auf Spielfeld
+        return row >= 0 && row < spielfeld1.length && col >= 0 && col < spielfeld1[0].length;
     }
     // Bombe Platzieren
 }
 
-
-//TODO: übrige Boote geht noch nicht 
+//TODO: Uebrige Anzahl korrekt berechnen
